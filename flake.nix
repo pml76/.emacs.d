357a3c0ba153a68ebefa0df633cb322c545014a8
Peter Lennartz <peter.lennartz@protonmail.com>
2026-03-30 21:32:07 +0200

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+  
+    emacs-init-el = ./init.el
+    emacs-early-init-el = ./early-init.el
+
+  };
+}
