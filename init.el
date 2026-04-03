9c6fbc6a96fe2ab759ed51d472abef912770762b
pml76 <peter.lennartz@gmail.com>
2026-04-03 12:43:07 +0200

    added cape completion

--- init.el
+++ init.el
@@ -54,7 +54,7 @@
     (add-to-list 'default-frame-alist '(alpha . (95 . 100)))
     (message "Running on Windows"))
    ((eq system-type 'gnu/linux)
-    (set-frame-parameter nil 'alpha-background 95) 
+    (set-frame-parameter nil 'alpha-background 95)
     (add-to-list 'default-frame-alist '(alpha-background . 95))
     (message "Running on GNU/Linux"))
    (t
@@ -230,6 +230,29 @@
   ;; (corfu-history-mode)
   ;; (corfu-popupinfo-mode)
   )
+
+;; Add extensions
+(use-package cape
+  :straight t
+  ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
+  ;; Press C-c p ? to for help.
+  :bind ("C-c p" . cape-prefix-map) ;; Alternative key: M-<tab>, M-p, M-+
+  ;; Alternatively bind Cape commands individually.
+  ;; :bind (("C-c p d" . cape-dabbrev)
+  ;;        ("C-c p h" . cape-history)
+  ;;        ("C-c p f" . cape-file)
+  ;;        ...)
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  ;; (add-hook 'completion-at-point-functions #'cape-history)
+  ;; ...
+)
 
 ;; Use Dabbrev with Corfu!
 (use-package dabbrev
