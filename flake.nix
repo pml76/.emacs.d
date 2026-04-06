2fa23eb3049250d5f16a864b75c840d119c84997
Peter Lennartz <peter.lennartz[at]gmail.com>
2026-04-06 13:17:27 +0200

    .

--- flake.nix
+++ flake.nix
@@ -16,57 +16,55 @@
       system = "x86_64-linux";
       overlays = [ (import emacs-overlay) ];
     };
-    emacs-fields = {
-      name = pkgs.emacs-unstable-pgtk.name; 
-      src = pkgs.emacs-unstable-pgtk.src;
+    emacs-additional-packages = with pkgs; [
+      gcc
       
-      buildInputs = with pkgs; [
-        emacs-unstable-pgtk
-        gcc
+      # used by emacs
+      ripgrep
       
-        # used by emacs
-        ripgrep
-
-        # used by emacs for c++ development
-        clang
-        lldb
-        clang-tools
-        cmakeWithGui
-        ccls
-        gdb
-        semgrep
-        python3
-
-        # used by emacs for nix development
-        nixd
-
-        # fonts
-        noto-fonts
-        noto-fonts-cjk-sans
-        noto-fonts-color-emoji
-        liberation_ttf
-        fira-code
-        fira-code-symbols
-        mplus-outline-fonts.githubRelease
-        dina-font
-        cantarell-fonts
-        #    all-the-icons
-        emacs-all-the-icons-fonts
+      # used by emacs for c++ development
+      clang
+      lldb
+      clang-tools
+      cmakeWithGui
+      ccls
+      gdb
+      semgrep
+      python3
+      
+      # used by emacs for nix development
+      nixd
+      
+      # fonts
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts.githubRelease
+      dina-font
+      cantarell-fonts
+      #    all-the-icons
+      emacs-all-the-icons-fonts
         
-      ] # nerd-fonts
-      ++ (builtins.filter
-        lib.attrsets.isDerivation
-        (builtins.attrValues pkgs.nerd-fonts))
-      ++ pkgs.emacs-unstable-pgtk.buildInputs;
-    };
-      
-  in{
+    ] # nerd-fonts
+    ++ builtins.filter
+      lib.attrsets.isDerivation
+      (builtins.attrValues pkgs.nerd-fonts);  
+  in {
   
     emacs-init-el = ./init.el;
     emacs-early-init-el = ./early-init.el;
 
-    configured-emacs = with pkgs; stdenv.mkDerivation emacs-fields;
-    devShells.x86_64-linux.default = pkgs.mkShell emacs-fields;
+    emacs-additional-packages = emacs-additional-packages;
+    
+    devShells.x86_64-linux.default = pkgs.mkShell {
+
+      buildInputs = with pkgs; [
+        emacs-unstable-pgtk ]
+      ++ emacs-additional-packages;
     };
+  };
 
 }
