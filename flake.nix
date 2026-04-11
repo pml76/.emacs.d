4345be47a68b0216a71aa57e017a9ec08355381a
Peter Lennartz <peter.lennartz@protonmail.com>
2026-04-11 07:50:06 +0200

    .

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
   outputs = { self, nixpkgs, emacs-overlay }: let
     pkgs = import nixpkgs {
       system = "x86_64-linux";
+      config = { allowUnfree = true; };
       overlays = [ (import emacs-overlay) ];
     };
     emacs-additional-packages = with pkgs; [
