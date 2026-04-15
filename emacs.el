5e03c55000304532efc07d9500f4d857087f1fc6
Peter Lennartz <peter.lennartz@protonmail.com>
2026-04-15 09:20:09 +0200

    .

--- emacs.el
+++ emacs.el
@@ -19,8 +19,6 @@
 
 
 
-(defvar runemacs/default-font-size 180)
-
 (setq inhibit-startup-message t)
 
 (scroll-bar-mode -1)        ; Disable visible scrollbar
@@ -29,4 +27,64 @@
 (set-fringe-mode 10)        ; Give some breathing room
 
 (menu-bar-mode -1)          ; Disable the menu bar
+
+
+
+(defvar pl/default-font-size 140)
+(defvar pl/default-font "JetBrains Mono")
+(defvar pl/variable-pitch-font "Source Serif 4")
+(defvar pl/variable-pitch-font-size 140)
+(defvar pl/fixed-pitch-font "JetBrains Mono")
+(defvar pl/fixed-pitch-font-size 140)
+
+(message "Setting faces!")
+
+(when (member pl/default-font (font-family-list))
+  (set-face-attribute 'default nil
+		      :font pl/default-font
+		      :height pl/default-font-size))
+
+(use-package mixed-pitch
+  :straight t
+  :config
+  (when (member pl/variable-pitch-font (font-family-list))
+    (set-face-attribute 'variable-pitch nil
+			:family pl/variable-pitch-font
+			:height pl/variable-pitch-font-size)))
+
+(when (member pl/fixed-pitch-font (font-family-list))
+  (set-face-attribute 'fixed-pitch nil
+		      :font pl/fixed-pitch-font
+		      :height pl/fixed-pitch-font-size))
+
+
+
+(defun pl/set-transparency ()
+"Set the transparency according to the platform."
+(cond
+ ((eq system-type 'windows-nt)
+  (set-frame-parameter (selected-frame) 'alpha '(95 . 100))
+  (add-to-list 'default-frame-alist '(alpha . (95 . 100)))
+  (message "Running on Windows"))
+ ((eq system-type 'gnu/linux)
+  (set-frame-parameter nil 'alpha-background 95)
+  (add-to-list 'default-frame-alist '(alpha-background . 95))
+  (message "Running on GNU/Linux"))
+ (t
+  (message "Unknown operating system"))))
+
+
+
+(defun pl/setup-gui ()
+  "Setup the gui."
+  (pl/set-transparency))
+
+(if (daemonp)
+    (add-hook 'after-make-frame-functions
+	      (lambda (frame)
+		;; (setq doom-modeline-icon t)
+		(with-selected-frame frame
+		  (pl/setup-gui)
+		  )))
+  (pl/setup-gui))
 
--- init.el
+++ init.el
@@ -315,59 +315,59 @@
 (use-package consult
   :straight t
   ;; Replace bindings. Lazily loaded by `use-package'.
-  :bind (;; C-c bindings in `mode-specific-map'
-         ("C-c M-x" . consult-mode-command)
-         ("C-c h" . consult-history)
-         ("C-c k" . consult-kmacro)
-         ("C-c m" . consult-man)
-         ("C-c i" . consult-info)
-         ([remap Info-search] . consult-info)
-         ;; C-x bindings in `ctl-x-map'
-         ("C-x M-:" . consult-complex-command) ;; orig. repeat-complex-command
-         ("C-x b" . consult-buffer) ;; orig. switch-to-buffer
-         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
-         ("C-x 5 b" . consult-buffer-other-frame) ;; orig. switch-to-buffer-other-frame
-         ("C-x t b" . consult-buffer-other-tab)	;; orig. switch-to-buffer-other-tab
-         ("C-x r b" . consult-bookmark)		;; orig. bookmark-jump
-         ("C-x p b" . consult-project-buffer) ;; orig. project-switch-to-buffer
-         ;; Custom M-# bindings for fast register access
-         ("M-#" . consult-register-load)
-         ("M-'" . consult-register-store) ;; orig. abbrev-prefix-mark (unrelated)
-         ("C-M-#" . consult-register)
-         ;; Other custom bindings
-         ("M-y" . consult-yank-pop) ;; orig. yank-pop
-         ;; M-g bindings in `goto-map'
-         ("M-g e" . consult-compile-error)
-         ("M-g r" . consult-grep-match)
-         ("M-g f" . consult-flymake) ;; Alternative: consult-flycheck
-         ("M-g g" . consult-goto-line)	 ;; orig. goto-line
-         ("M-g M-g" . consult-goto-line) ;; orig. goto-line
-         ("M-g o" . consult-outline) ;; Alternative: consult-org-heading
-         ("M-g m" . consult-mark)
-         ("M-g k" . consult-global-mark)
-         ("M-g i" . consult-imenu)
-         ("M-g I" . consult-imenu-multi)
-         ;; M-s bindings in `search-map'
-         ("M-s d" . consult-find) ;; Alternative: consult-fd
-         ("M-s c" . consult-locate)
-         ("M-s g" . consult-grep)
-         ("M-s G" . consult-git-grep)
-         ("M-s r" . consult-ripgrep)
-         ("M-s l" . consult-line)
-         ("M-s L" . consult-line-multi)
-         ("M-s k" . consult-keep-lines)
-         ("M-s u" . consult-focus-lines)
-         ;; Isearch integration
-         ("M-s e" . consult-isearch-history)
-         :map isearch-mode-map
-         ("M-e" . consult-isearch-history) ;; orig. isearch-edit-string
-         ("M-s e" . consult-isearch-history) ;; orig. isearch-edit-string
-         ("M-s l" . consult-line) ;; needed by consult-line to detect isearch
-         ("M-s L" . consult-line-multi)	;; needed by consult-line to detect isearch
-         ;; Minibuffer history
-         :map minibuffer-local-map
-         ("M-s" . consult-history) ;; orig. next-matching-history-element
-         ("M-r" . consult-history)) ;; orig. previous-matching-history-element
+  :bind ;; C-c bindings in `mode-specific-map'
+  ("C-c M-x" . consult-mode-command)
+  ("C-c h" . consult-history)
+  ("C-c k" . consult-kmacro)
+  ("C-c m" . consult-man)
+  ("C-c i" . consult-info)
+  ([remap Info-search] . consult-info)
+  ;; C-x bindings in `ctl-x-map'
+  ("C-x M-:" . consult-complex-command) ;; orig. repeat-complex-command
+  ("C-x b" . consult-buffer) ;; orig. switch-to-buffer
+  ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+  ("C-x 5 b" . consult-buffer-other-frame) ;; orig. switch-to-buffer-other-frame
+  ("C-x t b" . consult-buffer-other-tab)	;; orig. switch-to-buffer-other-tab
+  ("C-x r b" . consult-bookmark)		;; orig. bookmark-jump
+  ("C-x p b" . consult-project-buffer) ;; orig. project-switch-to-buffer
+  ;; Custom M-# bindings for fast register access
+  ("M-#" . consult-register-load)
+  ("M-'" . consult-register-store) ;; orig. abbrev-prefix-mark (unrelated)
+  ("C-M-#" . consult-register)
+  ;; Other custom bindings
+  ("M-y" . consult-yank-pop) ;; orig. yank-pop
+  ;; M-g bindings in `goto-map'
+  ("M-g e" . consult-compile-error)
+  ("M-g r" . consult-grep-match)
+  ("M-g f" . consult-flymake) ;; Alternative: consult-flycheck
+  ("M-g g" . consult-goto-line)	 ;; orig. goto-line
+  ("M-g M-g" . consult-goto-line) ;; orig. goto-line
+  ("M-g o" . consult-outline) ;; Alternative: consult-org-heading
+  ("M-g m" . consult-mark)
+  ("M-g k" . consult-global-mark)
+  ("M-g i" . consult-imenu)
+  ("M-g I" . consult-imenu-multi)
+  ;; M-s bindings in `search-map'
+  ("M-s d" . consult-find) ;; Alternative: consult-fd
+  ("M-s c" . consult-locate)
+  ("M-s g" . consult-grep)
+  ("M-s G" . consult-git-grep)
+  ("M-s r" . consult-ripgrep)
+  ("M-s l" . consult-line)
+  ("M-s L" . consult-line-multi)
+  ("M-s k" . consult-keep-lines)
+  ("M-s u" . consult-focus-lines)
+  ;; Isearch integration
+  ("M-s e" . consult-isearch-history)
+  :map isearch-mode-map
+  ("M-e" . consult-isearch-history) ;; orig. isearch-edit-string
+  ("M-s e" . consult-isearch-history) ;; orig. isearch-edit-string
+  ("M-s l" . consult-line) ;; needed by consult-line to detect isearch
+  ("M-s L" . consult-line-multi)	;; needed by consult-line to detect isearch
+  ;; Minibuffer history
+  :map minibuffer-local-map
+  ("M-s" . consult-history) ;; orig. next-matching-history-element
+  ("M-r" . consult-history) ;; orig. previous-matching-history-element
 
   ;; The :init configuration is always executed (Not lazy)
   :init
@@ -411,6 +411,44 @@
   ;; You may want to use `embark-prefix-help-command' or which-key instead.
   ;; (keymap-set consult-narrow-map (concat consult-narrow-key " ?") #'consult-narrow-help)
   )
+
+(use-package embark
+  :straight t
+
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc. You may adjust the
+  ;; Eldoc strategy, if you want to see the documentation from
+  ;; multiple providers. Beware that using this can be a little
+  ;; jarring since the message shown in the minibuffer can be more
+  ;; than one line, causing the modeline to move up and down:
+
+  ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  ;; Add Embark to the mouse context menu. Also enable `context-menu-mode'.
+  ;; (context-menu-mode 1)
+  ;; (add-hook 'context-menu-functions #'embark-context-menu 100)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :straight t)
 
 ;; Treemacs --------------------------------------------------------------------
 
