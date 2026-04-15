eed874c183952c4537eed454ce8ecd936a18beb3
Peter Lennartz <peter.lennartz@protonmail.com>
2026-04-15 16:18:04 +0200

    .

--- init.el
+++ init.el
@@ -412,43 +412,43 @@
   ;; (keymap-set consult-narrow-map (concat consult-narrow-key " ?") #'consult-narrow-help)
   )
 
-(use-package embark
-  :straight t
+;; (use-package embark
+;;   :straight t
 
-  :bind
-  (("C-." . embark-act)         ;; pick some comfortable binding
-   ("C-;" . embark-dwim)        ;; good alternative: M-.
-   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+;;   :bind
+;;   (("C-." . embark-act)         ;; pick some comfortable binding
+;;    ("C-;" . embark-dwim)        ;; good alternative: M-.
+;;    ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
 
-  :init
+;;   :init
 
-  ;; Optionally replace the key help with a completing-read interface
-  (setq prefix-help-command #'embark-prefix-help-command)
+;;   ;; Optionally replace the key help with a completing-read interface
+;;   (setq prefix-help-command #'embark-prefix-help-command)
 
-  ;; Show the Embark target at point via Eldoc. You may adjust the
-  ;; Eldoc strategy, if you want to see the documentation from
-  ;; multiple providers. Beware that using this can be a little
-  ;; jarring since the message shown in the minibuffer can be more
-  ;; than one line, causing the modeline to move up and down:
+;;   ;; Show the Embark target at point via Eldoc. You may adjust the
+;;   ;; Eldoc strategy, if you want to see the documentation from
+;;   ;; multiple providers. Beware that using this can be a little
+;;   ;; jarring since the message shown in the minibuffer can be more
+;;   ;; than one line, causing the modeline to move up and down:
 
-  ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
-  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+;;   ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+;;   ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
 
-  ;; Add Embark to the mouse context menu. Also enable `context-menu-mode'.
-  ;; (context-menu-mode 1)
-  ;; (add-hook 'context-menu-functions #'embark-context-menu 100)
+;;   ;; Add Embark to the mouse context menu. Also enable `context-menu-mode'.
+;;   ;; (context-menu-mode 1)
+;;   ;; (add-hook 'context-menu-functions #'embark-context-menu 100)
 
-  :config
+;;   :config
 
-  ;; Hide the mode line of the Embark live/completions buffers
-  (add-to-list 'display-buffer-alist
-               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
-                 nil
-                 (window-parameters (mode-line-format . none)))))
+;;   ;; Hide the mode line of the Embark live/completions buffers
+;;   (add-to-list 'display-buffer-alist
+;;                '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+;;                  nil
+;;                  (window-parameters (mode-line-format . none)))))
 
-;; Consult users will also want the embark-consult package.
-(use-package embark-consult
-  :straight t)
+;; ;; Consult users will also want the embark-consult package.
+;; (use-package embark-consult
+;;   :straight t)
 
 ;; Treemacs --------------------------------------------------------------------
 
@@ -692,211 +692,211 @@
 
 ;; Org Mode Configuration ------------------------------------------------------
 
-(defun pl/org-font-setup ()
-  ;; Replace list hyphen with dot
-  (font-lock-add-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+;; (defun pl/org-font-setup ()
+;;   ;; Replace list hyphen with dot
+;;   (font-lock-add-keywords 'org-mode
+;;                           '(("^ *\\([-]\\) "
+;;                              (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
-  ;; Set faces for heading levels
-  (dolist (face '((org-level-1 . 1.2)
-                  (org-level-2 . 1.1)
-                  (org-level-3 . 1.05)
-                  (org-level-4 . 1.0)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+;;   ;; Set faces for heading levels
+;;   (dolist (face '((org-level-1 . 1.2)
+;;                   (org-level-2 . 1.1)
+;;                   (org-level-3 . 1.05)
+;;                   (org-level-4 . 1.0)
+;;                   (org-level-5 . 1.1)
+;;                   (org-level-6 . 1.1)
+;;                   (org-level-7 . 1.1)
+;;                   (org-level-8 . 1.1)))
+;;     (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
 
-  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
-  (set-face-attribute 'org-block nil :foreground 'unspecified :inherit 'fixed-pitch)
-  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+;;   ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+;;   (set-face-attribute 'org-block nil :foreground 'unspecified :inherit 'fixed-pitch)
+;;   (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+;;   (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+;;   (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+;;   (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+;;   (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+;;   (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
 
-(defun pl/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  (visual-line-mode 1))
+;; (defun pl/org-mode-setup ()
+;;   (org-indent-mode)
+;;   (variable-pitch-mode 1)
+;;   (visual-line-mode 1))
 
 
-(use-package org
-  :straight t
-  :after consult
-  :hook (org-mode . pl/org-mode-setup)
-  :custom
+;; (use-package org
+;;   :straight t
+;;   :after consult
+;;   :hook (org-mode . pl/org-mode-setup)
+;;   :custom
   
-  ((org-agenda-start-with-log-mode t)
-   (org-ellipsis " ▾")                  
-   (org-agenda-files
-    '("~/org-agenda/Tasks.org"
-      "~/org-agenda/Habits.org"))
+;;   ((org-agenda-start-with-log-mode t)
+;;    (org-ellipsis " ▾")                  
+;;    (org-agenda-files
+;;     '("~/org-agenda/Tasks.org"
+;;       "~/org-agenda/Habits.org"))
    
-   (org-log-done 'time)                
-   (org-log-into-drawer t)
-   (org-todo-keywords
-    '((sequence "TODO(t)" "NEXT(n)" "|"  "DONE(!d)")
-      (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
+;;    (org-log-done 'time)                
+;;    (org-log-into-drawer t)
+;;    (org-todo-keywords
+;;     '((sequence "TODO(t)" "NEXT(n)" "|"  "DONE(!d)")
+;;       (sequence "BACKLOG(b)" "PLAN(p)" "READY(r)" "ACTIVE(a)" "REVIEW(v)" "WAIT(w@/!" "HOLD(h)" "|" "COMPLETED(c)" "CANC(k@)")))
 
-   (org-tag-alist
-    '((:startgroup)
-       ; Put mutually exclusive tags here
-       (:endgroup)
-       ("@errand" . ?E)
-       ("@home" . ?H)
-       ("@work" . ?W)
-       ("agenda" . ?a)
-       ("planning" . ?p)
-       ("publish" . ?P)
-       ("batch" . ?b)
-       ("note" . ?n)
-       ("idea" . ?i)))
+;;    (org-tag-alist
+;;     '((:startgroup)
+;;        ; Put mutually exclusive tags here
+;;        (:endgroup)
+;;        ("@errand" . ?E)
+;;        ("@home" . ?H)
+;;        ("@work" . ?W)
+;;        ("agenda" . ?a)
+;;        ("planning" . ?p)
+;;        ("publish" . ?P)
+;;        ("batch" . ?b)
+;;        ("note" . ?n)
+;;        ("idea" . ?i)))
 
-   (org-refile-targets
-    '(("~/Org-Agenda/Archive.org" :maxlevel . 1)))
+;;    (org-refile-targets
+;;     '(("~/Org-Agenda/Archive.org" :maxlevel . 1)))
 
    
-   (org-agenda-custom-commands
-    '(("d" "Dashboard"
-       ((agenda "" ((org-deadline-warning-days 7)))
-	(todo "NEXT"
-              ((org-agenda-overriding-header "Next Tasks")))
-	(tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
+;;    (org-agenda-custom-commands
+;;     '(("d" "Dashboard"
+;;        ((agenda "" ((org-deadline-warning-days 7)))
+;; 	(todo "NEXT"
+;;               ((org-agenda-overriding-header "Next Tasks")))
+;; 	(tags-todo "agenda/ACTIVE" ((org-agenda-overriding-header "Active Projects")))))
 
-      ("n" "Next Tasks"
-       ((todo "NEXT"
-              ((org-agenda-overriding-header "Next Tasks")))))
+;;       ("n" "Next Tasks"
+;;        ((todo "NEXT"
+;;               ((org-agenda-overriding-header "Next Tasks")))))
 
-      ("W" "Work Tasks" tags-todo "+work-email")
+;;       ("W" "Work Tasks" tags-todo "+work-email")
 
-      ;; Low-effort next actions
-      ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
-       ((org-agenda-overriding-header "Low Effort Tasks")
-	(org-agenda-max-todos 20)
-	(org-agenda-files org-agenda-files)))
+;;       ;; Low-effort next actions
+;;       ("e" tags-todo "+TODO=\"NEXT\"+Effort<15&+Effort>0"
+;;        ((org-agenda-overriding-header "Low Effort Tasks")
+;; 	(org-agenda-max-todos 20)
+;; 	(org-agenda-files org-agenda-files)))
 
-      ("w" "Workflow Status"
-       ((todo "WAIT"
-              ((org-agenda-overriding-header "Waiting on External")
-               (org-agenda-files org-agenda-files)))
-	(todo "REVIEW"
-              ((org-agenda-overriding-header "In Review")
-               (org-agenda-files org-agenda-files)))
-	(todo "PLAN"
-              ((org-agenda-overriding-header "In Planning")
-               (org-agenda-todo-list-sublevels nil)
-               (org-agenda-files org-agenda-files)))
-	(todo "BACKLOG"
-              ((org-agenda-overriding-header "Project Backlog")
-               (org-agenda-todo-list-sublevels nil)
-               (org-agenda-files org-agenda-files)))
-	(todo "READY"
-              ((org-agenda-overriding-header "Ready for Work")
-               (org-agenda-files org-agenda-files)))
-	(todo "ACTIVE"
-              ((org-agenda-overriding-header "Active Projects")
-               (org-agenda-files org-agenda-files)))
-	(todo "COMPLETED"
-              ((org-agenda-overriding-header "Completed Projects")
-               (org-agenda-files org-agenda-files)))
-	(todo "CANC"
-              ((org-agenda-overriding-header "Cancelled Projects")
-               (org-agenda-files org-agenda-files)))))))
+;;       ("w" "Workflow Status"
+;;        ((todo "WAIT"
+;;               ((org-agenda-overriding-header "Waiting on External")
+;;                (org-agenda-files org-agenda-files)))
+;; 	(todo "REVIEW"
+;;               ((org-agenda-overriding-header "In Review")
+;;                (org-agenda-files org-agenda-files)))
+;; 	(todo "PLAN"
+;;               ((org-agenda-overriding-header "In Planning")
+;;                (org-agenda-todo-list-sublevels nil)
+;;                (org-agenda-files org-agenda-files)))
+;; 	(todo "BACKLOG"
+;;               ((org-agenda-overriding-header "Project Backlog")
+;;                (org-agenda-todo-list-sublevels nil)
+;;                (org-agenda-files org-agenda-files)))
+;; 	(todo "READY"
+;;               ((org-agenda-overriding-header "Ready for Work")
+;;                (org-agenda-files org-agenda-files)))
+;; 	(todo "ACTIVE"
+;;               ((org-agenda-overriding-header "Active Projects")
+;;                (org-agenda-files org-agenda-files)))
+;; 	(todo "COMPLETED"
+;;               ((org-agenda-overriding-header "Completed Projects")
+;;                (org-agenda-files org-agenda-files)))
+;; 	(todo "CANC"
+;;               ((org-agenda-overriding-header "Cancelled Projects")
+;;                (org-agenda-files org-agenda-files)))))))
 
-   (org-capture-templates
-	 `(("t" "Tasks / Projects")
-	   ("tt" "Task" entry (file+olp "~/Org-Agenda/Tasks.org" "Inbox")
-            "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
+;;    (org-capture-templates
+;; 	 `(("t" "Tasks / Projects")
+;; 	   ("tt" "Task" entry (file+olp "~/Org-Agenda/Tasks.org" "Inbox")
+;;             "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
 
-	   ("j" "Journal Entries")
-	   ("jj" "Journal" entry
-            (file+olp+datetree "~/Org-Agenda/Journal.org")
-            "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
-            ;; ,(dw/read-file-as-string "~/Notes/Templates/Daily.org")
-            :clock-in :clock-resume
-            :empty-lines 1)
-	   ("jm" "Meeting" entry
-            (file+olp+datetree "~/Org-Agenda/Journal.org")
-            "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
-            :clock-in :clock-resume
-            :empty-lines 1)
+;; 	   ("j" "Journal Entries")
+;; 	   ("jj" "Journal" entry
+;;             (file+olp+datetree "~/Org-Agenda/Journal.org")
+;;             "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
+;;             ;; ,(dw/read-file-as-string "~/Notes/Templates/Daily.org")
+;;             :clock-in :clock-resume
+;;             :empty-lines 1)
+;; 	   ("jm" "Meeting" entry
+;;             (file+olp+datetree "~/Org-Agenda/Journal.org")
+;;             "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
+;;             :clock-in :clock-resume
+;;             :empty-lines 1)
 
-	   ("w" "Workflows")
-	   ("we" "Checking Email" entry (file+olp+datetree "~/Org-Agenda/Journal.org")
-            "* Checking Email :email:\n\n%?" :clock-in :clock-resume :empty-lines 1)
+;; 	   ("w" "Workflows")
+;; 	   ("we" "Checking Email" entry (file+olp+datetree "~/Org-Agenda/Journal.org")
+;;             "* Checking Email :email:\n\n%?" :clock-in :clock-resume :empty-lines 1)
 
-	   ("m" "Metrics Capture")
-	   ("mw" "Weight" table-line (file+headline "~/Org-Agenda/Metrics.org" "Weight")
-	    "| %U | %^{Weight} | %^{Notes} |" :kill-buffer t)))
+;; 	   ("m" "Metrics Capture")
+;; 	   ("mw" "Weight" table-line (file+headline "~/Org-Agenda/Metrics.org" "Weight")
+;; 	    "| %U | %^{Weight} | %^{Notes} |" :kill-buffer t)))
 
-   (setq org-habit-graph-column 60))
+;;    (setq org-habit-graph-column 60))
   
-  :config
+;;   :config
 
-  (require 'org-tempo)
-  (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
-  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
-  (add-to-list 'org-structure-template-alist '("py" . "src python"))
-  (add-to-list 'org-structure-template-alist '("cc" . "src cpp"))
-  (add-to-list 'org-structure-template-alist '("hs" . "src haskell"))
-  (add-to-list 'org-structure-template-alist '("co" . "src coq"))
-  (add-to-list 'org-structure-template-alist '("rs" . "src rustic"))
+;;   (require 'org-tempo)
+;;   (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+;;   (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+;;   (add-to-list 'org-structure-template-alist '("py" . "src python"))
+;;   (add-to-list 'org-structure-template-alist '("cc" . "src cpp"))
+;;   (add-to-list 'org-structure-template-alist '("hs" . "src haskell"))
+;;   (add-to-list 'org-structure-template-alist '("co" . "src coq"))
+;;   (add-to-list 'org-structure-template-alist '("rs" . "src rustic"))
   
-  (require 'org-habit)
-  (add-to-list 'org-modules 'org-habit)
+;;   (require 'org-habit)
+;;   (add-to-list 'org-modules 'org-habit)
   
-  (define-key global-map (kbd "C-c j")
-	      (lambda () (interactive) (org-capture nil "jj")))
+;;   (define-key global-map (kbd "C-c j")
+;; 	      (lambda () (interactive) (org-capture nil "jj")))
   
-  (pl/org-font-setup)
+;;   (pl/org-font-setup)
 
-  ;; save buffers after refiling
-  (advice-add 'org-refile :after 'org-save-all-org-buffers)
-
-
-  ;; Ensure rustic is loaded and mapped to 'rust'
-  (add-to-list 'org-src-lang-modes '("rust" . rustic))
-
-  ;; Optional: Set the tangle extension to 'rs' for rustic
-  (add-to-list 'org-babel-tangle-lang-exts '("rustic" . "rs"))
-
-  ;; Optional: Alias the execute function to use rustic's implementation
-  (defalias 'org-babel-execute:rust #'org-babel-execute:rustic))
-
-(use-package org-contrib
-  :straight t)
-
-(use-package org-bullets
-  :straight t
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
-(defun pl/org-mode-visual-fill ()
-  (setq visual-fill-column-width 100
-        visual-fill-column-center-text t)
-  (visual-fill-column-mode 1))
-
-(use-package visual-fill-column
-  :straight t
-  :hook (org-mode . pl/org-mode-visual-fill))
-
-;; org-store-link, org-agenda, and org-capture should be
-;; reachable from anywhere
-(global-set-key (kbd "C-c l") #'org-store-link)
-(global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "C-c c") #'org-capture)
+;;   ;; save buffers after refiling
+;;   (advice-add 'org-refile :after 'org-save-all-org-buffers)
 
 
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((emacs-lisp . t)
-   (python . t)))
+;;   ;; Ensure rustic is loaded and mapped to 'rust'
+;;   (add-to-list 'org-src-lang-modes '("rust" . rustic))
+
+;;   ;; Optional: Set the tangle extension to 'rs' for rustic
+;;   (add-to-list 'org-babel-tangle-lang-exts '("rustic" . "rs"))
+
+;;   ;; Optional: Alias the execute function to use rustic's implementation
+;;   (defalias 'org-babel-execute:rust #'org-babel-execute:rustic))
+
+;; (use-package org-contrib
+;;   :straight t)
+
+;; (use-package org-bullets
+;;   :straight t
+;;   :after org
+;;   :hook (org-mode . org-bullets-mode)
+;;   :custom
+;;   (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+;; (defun pl/org-mode-visual-fill ()
+;;   (setq visual-fill-column-width 100
+;;         visual-fill-column-center-text t)
+;;   (visual-fill-column-mode 1))
+
+;; (use-package visual-fill-column
+;;   :straight t
+;;   :hook (org-mode . pl/org-mode-visual-fill))
+
+;; ;; org-store-link, org-agenda, and org-capture should be
+;; ;; reachable from anywhere
+;; (global-set-key (kbd "C-c l") #'org-store-link)
+;; (global-set-key (kbd "C-c a") #'org-agenda)
+;; (global-set-key (kbd "C-c c") #'org-capture)
+
+
+;; (org-babel-do-load-languages
+;;  'org-babel-load-languages
+;;  '((emacs-lisp . t)
+;;    (python . t)))
 
 
 ;; rustic ----------------------------------------------------
